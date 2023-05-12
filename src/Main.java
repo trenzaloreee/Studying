70caa49c66f0e9a2587284f968c59b79c11913e8
danitsoi <danitsoi@mail.ru>
2023-05-12 17:38:03 +0300

    third

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello!!!!!!!");
+        System.out.println("Hello!");
 //        int[] arr = {1, 2, 3, 4, 5, 6};
 //        System.out.println(Arrays.toString(reverseArray(arr)));
     }
